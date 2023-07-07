94fe1728f397bbc4a92bd3171d90fac7c2adb95f
JaroslawBR <JAROS3456@GMAIL.COM>
2023-07-07 17:30:52 +0200

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -34,22 +34,21 @@ class Blockchain {
         val blockNum = 5
 
         for (i in 1..blockNum) {
-            var magickNumb = 0
-            var newBlock: Block
+            var magicNumb = 0
             val startTime = System.currentTimeMillis()
             val timestamp = System.currentTimeMillis()
+            val lastBlockHash = block.lastOrNull()?.hash ?: "0"
+            val conditionPrefix = "0".repeat(zeroes)
 
-            while (true) {
-                val sha256 = applySha256("$i$timestamp${block.lastOrNull()?.hash ?: 0}$magickNumb")
-                if (sha256.substring(0 until zeroes) == "0".repeat(zeroes)) {
-                    val endTime: Int = ((System.currentTimeMillis() - startTime) / 1000).toInt()
-                    newBlock = Block(block.size + 1, timestamp, sha256, endTime, magickNumb)
-                    break
-                }
-                magickNumb ++
+            while (!applySha256("$i$timestamp$lastBlockHash$magicNumb").startsWith(conditionPrefix)) {
+                magicNumb++
             }
+
+            val endTime = ((System.currentTimeMillis() - startTime) / 1000).toInt()
+            val newBlock = Block(block.size + 1, timestamp, applySha256("$i$timestamp$lastBlockHash$magicNumb"), endTime, magicNumb)
             block.add(newBlock)
         }
+
     }
 
     private fun show(){
